370fd3a3084e9f850e1205f7fa9094e007e4dd6b
MikoStellio <134621707+MikoStellio@users.noreply.github.com>
2023-06-07 01:05:33 +0800

    PARALLAX UPDATE!
    - added parallax effect for about.html
    - added responsive siziing for ~960px to 1400px screens
    - fixed index.html meta tags
    
    other notes:
    - still needs a universal styling
    - needs cleaning of codes especially about.css
    - work on contacts page!
    - study react.js for responsive design

--- js/parallax.js
+++ js/parallax.js
@@ -1,11 +1,28 @@
-let myName = document.getElementById('name');
-let titleDev = document.getElementById('title1');
-let titleDes = document.getElementById('title2');
-let dpBox = document.getElementById('profile-picture-border');
-let dpCont = document.getElementById('profile-picture-container');
-let dp = document.getElementById('profile-picture');
+const parallaxElements = document.querySelectorAll(".parallax");
+const rotateElements = document.querySelectorAll(".rotate");
 
-window.addEventListener('scroll', functionn() {
-    let value = window.scrollY;
-    myName.style.left = value * .25 + 'px';
+let xValue = 0, 
+    yValue = 0;
+
+window.addEventListener("mousemove", (e) => {
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    parallaxElements.forEach(element => {
+        let speedx = element.dataset.speedx;
+        let speedy = element.dataset.speedy;
+
+        element.style.transform = 
+        `translateX(calc(-50% + ${-xValue * speedx}px)) 
+        translateY(calc(-50% + ${yValue * speedy}px))`;
+    })
+    
+    rotateElements.forEach(element => {
+        let speedx = element.dataset.speedx;
+        let speedy = element.dataset.speedy;
+
+        element.style.transform = `rotate(5deg)
+        translateX(calc(-50% + ${-xValue * speedx}px)) 
+        translateY(calc(-50% + ${yValue * speedy}px))`;
+    })
 })
